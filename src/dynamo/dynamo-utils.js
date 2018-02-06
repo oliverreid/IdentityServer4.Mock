bcf57aad58e3575665c5f7959766c48b70599348
Mark Tranter <mark.a.tranter@gmail/com>
2018-02-06 20:47:45 +1100

    Add removes to utils

--- src/dynamo/dynamo-utils.js
+++ src/dynamo/dynamo-utils.js
@@ -7,13 +7,14 @@ module.exports = {
     const serializePatch = (patch, path, init) => {
       path = path || ""
       init = init ||  {
-        UpdateExpression: "set ",
+        updates: [],
+        removes: [],
         ExpressionAttributeValues: {}
       };
       return Object.keys(patch).reduce((agg, next) => {
         const val = patch[next];
         const prop = `${path}${path.length > 0 ? "." : ""}${next}`
-        if(typeof val === 'object' && !Array.isArray(val)) {
+        if(typeof val === 'object' && !Array.isArray(val) && !!val) {
           return serializePatch(val, prop, agg)
         } else {
           const thisVar = nextVar();
@@ -23,14 +24,27 @@ module.exports = {
             agg.ExpressionAttributeNames = agg.ExpressionAttributeNames || {}
             agg.ExpressionAttributeNames[key] = prop;
           }
-          agg.UpdateExpression = agg.UpdateExpression + `${key}=${thisVar}, `
-          agg.ExpressionAttributeValues[thisVar] = val
+          if(val) {
+            agg.updates.push(`${key}=${thisVar}`)
+            agg.ExpressionAttributeValues[thisVar] = val
+          } else {
+            agg.removes.push(key)
+          }
           return agg;
         }
       }, init)
     }
     var retval = serializePatch(patch);
-    retval.UpdateExpression = retval.UpdateExpression.substring(0, retval.UpdateExpression.length - 2)
+    if(retval.updates.length){
+      retval.UpdateExpression = retval.updates.reduce((agg,n) => agg + n + ", ", "set ")
+      retval.UpdateExpression = retval.UpdateExpression.substring(0, retval.UpdateExpression.length - 2)
+    }
+    if(retval.removes.length){
+      retval.UpdateExpression += retval.removes.reduce((agg,n) => agg + n + ", ", " remove ")
+      retval.UpdateExpression = retval.UpdateExpression.substring(0, retval.UpdateExpression.length - 2)
+    }
+    delete retval.updates
+    delete retval.removes
     return retval;
   }
 }
